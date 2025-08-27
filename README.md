e5b42dafe58d84ad5346b702e0f9d1182e45f1d7
SoravitX <soravit.manan@gmail.com>
2025-08-27 15:25:35 +0700

    The first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Version control with Git & GitHub
+Psu Trang
+August 27, 2025
---
+++ TestGreeter.java
@@ -0,0 +1,6 @@
+public class TestGreeter {
+    public static void main (String[] args) {
+        Greet g = new Greet();
+        g.greet("John");
+    }
+}
